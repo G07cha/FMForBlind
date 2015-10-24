0148411b4dd3efd6517d7727070b473ae70cae06
Konstantin Azizov <azizovkostya97@gmail.com>
2015-10-24 13:56:41 +0300

    Add selectFrom function
    Asking for selection in array

--- voice.py
+++ voice.py
@@ -1,11 +1,31 @@
+import keylogger
+
 class Voice:
 	def __init__(self, options=False):
 		if options:
 			self.options = options
 	
-	""" Return numbers """
-	def numerableList(self, array):
-		""" Here will be pronounced list with numbers to select """
+	""" Pronounce list with numbers to select and then asked for selection """
+	def selectFrom(self, array):
+		while True:
+			index = 0
+			for item in array:
+				index += 1
+				self.voiceOutput(str(index) + ' ' + item)
+			
+			selectedIndex = raw_input('Enter item number: ')
+			
+			if selectedIndex:
+				selectedItem = array[int(selectedIndex) - 1]
+				self.voiceOutput('You have selected: ' + selectedItem + '. Is it correct?')
+				
+				isCorrect = keylogger.getkey()
+				
+				if isCorrect == 'y':
+					return selectedItem
+			else: 
+				return False
 		
 	def voiceOutput(self, out):
 		""" Pronounce given output """
+		print out
